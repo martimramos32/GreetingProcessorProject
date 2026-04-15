cb15273b70aa0786a96cdc394ffd9ef15ac0ab91
marti <martim.ramos2005@gmail.com>
2026-04-15 11:53:56 +0100

    Set up das dependências necessárias para cada um dos módulos criados.

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm")
+    kotlin("kapt") // Preferred in Kotlin DSL (Gradle KTS)
 }
 
 group = "org.example"
@@ -11,6 +12,19 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib"))
+    // AutoService to register the processor automatically
+    implementation("com.google.auto.service:auto-service:1.1.1")
+    // Required for registering the processor
+    kapt("com.google.auto.service:auto-service:1.1.1")
+    // KotlinPoet for generating Kotlin code
+    implementation("com.squareup:kotlinpoet:1.14.2")
+    // Include the annotations module
+    implementation(project(":annotations"))
+}
+
+kapt {
+    correctErrorTypes = true
 }
 
 kotlin {
